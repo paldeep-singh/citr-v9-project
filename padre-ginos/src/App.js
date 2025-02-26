9eb54da5f61757952323aecbe02333c51cc51454
Paldeep Singh <paldeepsingh21@gmail.com>
2025-02-26 18:51:47 +1100

    Prettier

---
+++ padre-ginos/src/App.js
@@ -0,0 +1,35 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement("h1", null, "Padre Ginos!"),
+    React.createElement(Pizza, {
+      name: "Pepperoni",
+      description: "Pepperoni, cheese, tomato sauce",
+    }),
+    React.createElement(Pizza, {
+      name: "Margherita",
+      description: "Cheese, tomato sauce",
+    }),
+    React.createElement(Pizza, {
+      name: "Hawaiian",
+      description: "Ham, pineapple, cheese, tomato sauce",
+    }),
+    React.createElement(Pizza, {
+      name: "Vegetarian",
+      description: "Mushrooms, peppers, onions, cheese, tomato sauce",
+    }),
+  );
+};
+
+const container = document.getElementById("root");
+
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App)); // Render App component inside root element in index.html
